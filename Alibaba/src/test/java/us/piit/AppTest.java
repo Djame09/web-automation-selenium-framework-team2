07ec0251e2b055dc23940a8132cb94730ade20a0
Djamel09 <djamelnaitallou@gmail.com>
2022-02-23 22:52:50 -0500

    add ignor file

--- Alibaba/src/test/java/us/piit/AppTest.java
+++ Alibaba/src/test/java/us/piit/AppTest.java
@@ -2,14 +2,12 @@ package us.piit;
 
 import base.CommonAPI;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AppTest extends CommonAPI {
 
-  // @Test
-    public void categoriesList(){
-        HomePage homePage= PageFactory.initElements(driver,HomePage.class);
-        homePage.hoverOverCategories(driver);
+
 
 
 
@@ -17,4 +15,4 @@ public class AppTest extends CommonAPI {
 
 
 
-}
+
